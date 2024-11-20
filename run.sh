ba11bf5e24b61a5256bd366f23827e162e14d61b
Georg Pernice <georgpernice@gmail.com>
2024-11-20 10:54:08 +0100

    updates

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 for ((i=0;i<10;i++))
 do
+	trap exit SIGINT
 	toilet -w $(tput cols) -f future -F gay "$(cat index.md)" | pv -lqL 10
 	sleep 1
 	clear
+	toilet -w $(tput cols) -f future -F metal "$(cat index.md)" | pv -lqL 10
+	sleep 1
+	clear
+	figlet -f future "Reset to origin main and pull new updates.."
+	git reset --hard origin main
+	git pull
+
 done
 
