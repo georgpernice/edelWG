f0c505bf18e522b095126403173bba081ce98a3f
Georg Pernice <georgpernice@gmail.com>
2024-11-20 10:57:01 +0100

    fix mds

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 for ((i=0;i<10;i++))
 do
 	trap exit SIGINT
-	toilet -w $(tput cols) -f future -F gay "$(cat index.md)" | pv -lqL 10
+	toilet -w $(tput cols) -f future -F gay "$(cat events.md)" | pv -lqL 10
 	sleep 1
 	clear
-	toilet -w $(tput cols) -f future -F metal "$(cat index.md)" | pv -lqL 10
+	toilet -w $(tput cols) -f future -F metal "$(cat shopping.md)" | pv -lqL 10
 	sleep 1
 	clear
 	figlet -f future "Reset to origin main and pull new updates.."
