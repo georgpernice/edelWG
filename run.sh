c3d34c19b94353f8a292965b30fe8a4f4501e7ed
Georg Pernice <georgpernice@gmail.com>
2024-11-20 11:06:04 +0100

    fix loop

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@ for ((i=0;9==9;i++))
 do
 	trap exit SIGINT
 	toilet -w $(tput cols) -f future -F gay "$(cat events.md)" | pv -lqL 10
-	sleep 2
+	sleep 3
 	clear
 	toilet -w $(tput cols) -f future -F metal "$(cat shopping.md)" | pv -lqL 10
-	sleep 2
+	sleep 3
 	clear
 	figlet -f bubble "Reset to origin main and pull new updates.."
 	git reset --hard origin/main
 	git pull
-
+	clear
 done
 
