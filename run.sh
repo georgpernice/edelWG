b1f66d7175408ecca21f9a253a62e41a62735b40
Georg Pernice <104216802+georgpernice@users.noreply.github.com>
2024-11-20 23:51:16 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 for ((i=0;9==9;i++))
 do
 	trap exit SIGINT
-	toilet -w $(tput cols) -f bigmono9 -F gay "$(cat events.md)" | pv -lqL 10
+	toilet -w $(tput cols) -f bigmono9 "$(cat events.md)" | pv -lqL 10
 	sleep 3
 	clear
-	toilet -w $(tput cols) -f bigmono9 -F metal "$(cat shopping.md)" | pv -lqL 10
+	toilet -w $(tput cols) -f bigmono9 "$(cat shopping.md)" | pv -lqL 10
 	sleep 3
 	clear
 	figlet -f future "Reset to origin main and pull new updates.."
