8fef211e92eb8992f11185fb87346673e02c1746
Georg Pernice <104216802+georgpernice@users.noreply.github.com>
2024-11-21 00:38:43 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,24 @@
+RED_B='\e[1;91m'
+GREEN_B='\e[1;92m'
+YELLOW_B='\e[1;93m'
+BLUE_B='\e[1;94m'
+PURPLE_B='\e[1;95m'
+CYAN_B='\e[1;96m'
+WHITE_B='\e[1;97m'
+RESET='\e[0m'
+
+red() { echo -e "${RED_B}${1}${RESET}"; }
+green() { echo -e "${GREEN_B}${1}${RESET}"; }
+yellow() { echo -e "${YELLOW_B}${1}${RESET}"; }
+blue() { echo -e "${BLUE_B}${1}${RESET}"; }
+purple() { echo -e "${PURPLE_B}${1}${RESET}"; }
+cyan() { echo -e "${CYAN_B}${1}${RESET}"; }
+white() { echo -e "${WHITE_B}${1}${RESET}"; }
+
 for ((i=0;9==9;i++))
 do
 	trap exit SIGINT
-	toilet -w $(tput cols) -f bigmono9 "$(cat events.md)" | pv -lqL 10
+	green "$(toilet -w $(tput cols) -f bigmono9 "$(cat events.md)" | pv -lqL 10)"
 	sleep 3
 	clear
 	toilet -w $(tput cols) -f bigmono9 "$(cat shopping.md)" | pv -lqL 10
