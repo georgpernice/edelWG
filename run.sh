1b4cb875b6af8892b98180c1b780b29bba85a887
Georg Pernice <104216802+georgpernice@users.noreply.github.com>
2025-02-04 12:13:00 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,38 +15,38 @@ purple() { echo -e "${PURPLE_B}${1}${RESET}"; }
 cyan() { echo -e "${CYAN_B}${1}${RESET}"; }
 white() { echo -e "${WHITE_B}${1}${RESET}"; }
 
-lastupdate=$(git show -s --date=format:'%d.%m.%Y' --format=%cd)
+lastupdate="no date found yet.."
 for ((i=0;9==9;i++))
 do
-	trap exit SIGINT
+        trap exit SIGINT
 
-  	# see if we update soon
-  	odd=$((i % 2))
- 	if [ $odd -eq 1 ]
- 	then
-	 	# update git repo
-   		figlet -f big "Reset to origin main and pull new updates.."
-	 	git reset --hard origin/main
-		git pull
-		clear
-  	fi
- 	# page1
-  	green "$(figlet -w $(tput cols) -f bigmono9 Edel-Homepage - Last updated on: $lastupdate)"
-   	green "$(figlet -w $(tput cols) -f bigmono9  ========================================== )"
-	red "$(figlet -w $(tput cols) -f bigmono9 "$(cat events.md)")" | pv -lqL 20
-	sleep 6
-	clear
- 	# page2
- 	green "$(figlet -w $(tput cols) -f bigmono9 Edel-Homepage - Last updated on: $lastupdate)"
-  	green "$(figlet -w $(tput cols) -f bigmono9  ========================================== )"
-	blue "$(figlet -w $(tput cols) -f bigmono9 "$(cat shopping.md)")" | pv -lqL 20
-	sleep 6
-	clear 
+        # see if we update soon
+        odd=$((i % 2))
+        if [ $odd -eq 1 ]
+        then
+                # update git repo
+                figlet -f big "Reset to origin main and pull new updates.."
+                git reset --hard origin/main
+                git pull
+                clear
+                lastupdate=$(git show -s --date=format:'%d.%m.%Y' --format=%cd)
+        fi
+        # page1
+        green "$(figlet -w $(tput cols) -f bigmono9 Edel-Homepage - Last updated on: $lastupdate)"
+        green "$(figlet -w $(tput cols) -f bigmono9  ========================================== )"
+        red "$(figlet -w $(tput cols) -f bigmono9 "$(cat events.md)")" | pv -lqL 20
+        sleep 6
+        clear
+        # page2
+        green "$(figlet -w $(tput cols) -f bigmono9 Edel-Homepage - Last updated on: $lastupdate)"
+        green "$(figlet -w $(tput cols) -f bigmono9  ========================================== )"
+        blue "$(figlet -w $(tput cols) -f bigmono9 "$(cat shopping.md)")" | pv -lqL 20
+        sleep 6
+        clear
 # page3
- 	green "$(figlet -w $(tput cols) -f bigmono9 Edel-Homepage - Last updated on: $lastupdate)"
-  	green "$(figlet -w $(tput cols) -f bigmono9  ========================================== )"
-	yellow "$(figlet -w $(tput cols) -f bigmono9 "$(cat msg.md)")" | pv -lqL 20
-	sleep 6
-	clear 
+        green "$(figlet -w $(tput cols) -f bigmono9 Edel-Homepage - Last updated on: $lastupdate)"
+        green "$(figlet -w $(tput cols) -f bigmono9  ========================================== )"
+        yellow "$(figlet -w $(tput cols) -f bigmono9 "$(cat msg.md)")" | pv -lqL 20
+        sleep 6
+        clear
 done
-
