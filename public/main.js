605884a99167092cfd84dbcb6f978bbc94931845
Georg Pernice <georgpernice@gmail.com>
2025-04-27 14:21:51 +0200

    upd

--- public/main.js
+++ public/main.js
@@ -1,39 +1,47 @@
-// **** AutoScroll + ScrollSpy ****
-var CORRECTION = 50;  // height of the navbar 
-// don't forget to setup the data-offset attribute of the <body> tag
+  let CORRECTION = 50;  // height of the navbar 
+  let DELAY_READING = 4000; // 4 seconds = 4000; 10 seconds = 10000
+  let DELAY_SCROLLING = 1500;
 
-var DELAY_READING = 1000; // 4 seconds = 4000; 10 seconds = 10000
-var DELAY_SCROLLING = 1500;
+  let links = ['#section-start', '#section-green', '#section-blue', '#section-red', '#section-stop'];
+  let timerId = 0;
 
-var links = [ '#section-start', '#section-green', '#section-blue', '#section-red', '#section-stop' ];
-var timerId = 0;
+  delayLinks(0);
 
-delayLinks(0);
-
-$( '#section-stop a' ).click(function(event) { 
-  event.preventDefault();
-  clearTimeout(timerId); 
-});
-
-$( '#navbar-1 li a' ).click(function(event) {
-  event.preventDefault();
-  scrollToLink( $(this).attr('href') );
-});
-
-function delayLinks( i ) {
-  if( i >= links.length ) i = 0;
-  scrollToLink( links[i] );
-  
-  var next = ( i == links.length - 1 ? 0 : i + 1);
-  timerId = setTimeout(function() { delayLinks( next ) }, DELAY_READING ); 
-}
-
-function scrollToLink( link ) {
-  selectLink = $( link );
-  if ( selectLink.length ) {
-    var top = selectLink.offset().top - CORRECTION;
-    $('body,html').stop().animate({scrollTop: top}, DELAY_SCROLLING);
-  } else {
-    console.log('The link is not found: ' + link);
+  // Stop autoscroll when clicking "Stop"
+  let stopLink = document.querySelector('#section-stop a');
+  if (stopLink) {
+    stopLink.addEventListener('click', function(event) { 
+      event.preventDefault();
+      clearTimeout(timerId); 
+    });
   }
-}
+
+  // Scroll when clicking navbar links
+  let navbarLinks = document.querySelectorAll('#navbar-1 li a');
+  navbarLinks.forEach(function(anchor) {
+    anchor.addEventListener('click', function(event) {
+      event.preventDefault();
+      scrollToLink(this.getAttribute('href'));
+    });
+  });
+
+  function delayLinks(i) {
+    if (i >= links.length) i = 0;
+    scrollToLink(links[i]);
+
+    let next = (i === links.length - 1 ? 0 : i + 1);
+    timerId = setTimeout(function() { delayLinks(next); }, DELAY_READING); 
+  }
+
+  function scrollToLink(link) {
+    let selectLink = document.querySelector(link);
+    if (selectLink) {
+      let top = selectLink.getBoundingClientRect().top + window.scrollY - CORRECTION;
+      window.scrollTo({
+        top: top,
+        behavior: 'smooth'
+      });
+    } else {
+      console.log('The link is not found: ' + link);
+    }
+  }
